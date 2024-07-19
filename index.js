046214710195d79be53efc081a7117acbba2f2d3
divyaaa374 <divya.singh9354@gmail.com>
2024-07-19 15:18:47 +0530

    sample commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartitem.classList.remove('active');
+}
+
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartitem.classList.remove('active');
+}
+
+let cartitem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart').onclick = () => {
+    cartitem.classList.toggle('active'); 
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+
+window.onscroll = ( ) =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartitem.classList.remove('active');
+}
